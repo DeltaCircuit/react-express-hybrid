d9649e2ba7a36319658ce7410d56d1ea838fa3c6
Giridharan GM <giridharangm@outlook.com>
2017-01-18 20:46:05 +0530

    WebpackDevServer moved to config folder.

---
+++ config/WebpackDevServer.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const WebpackDevServer = require('webpack-dev-server');
+const chalk = require('chalk');
+const compiler = require('./compiler');
+
+const devServer = new WebpackDevServer(compiler, {
+  hot: true,
+  quiet: true,
+});
+if (process.env.NODE_ENV !== 'production') {
+  devServer.listen(8080, () => {
+        // eslint-disable-next-line no-console
+    console.log(chalk.gray(`Webpack Dev Server started at ${8080}`));
+  });
+}
+module.exports = devServer.middleware;
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
-import WebpackDevServer from 'webpack-dev-server';
+// import WebpackDevServer from 'webpack-dev-server';
 // import opn from 'opn';
-import chalk from 'chalk';
+// import chalk from 'chalk';
 import path from 'path';
 import compiler from '../../config/compiler';
+import webpackMiddleware from '../../config/WebpackDevServer';
 // Initialize Express
 const app = express();
 
@@ -16,38 +17,25 @@ app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 
 // If no environment is specified, we'll assume its developments
 if (env !== 'production') {
-  // Fire up the Webpack Dev Server with HMR
-  const devServer = new WebpackDevServer(compiler, {
-    hot: true,
-    quiet: true,
-  });
-
-  app.use(devServer.middleware);
-
-  devServer.listen(8080, () => {
-    // eslint-disable-next-line no-console
-    console.log(chalk.gray(`Webpack Dev Server started at ${8080}`));
-  });
-
-  app.get('*', (req, res, next) => {
-    const reactApp = req.originalUrl;
-    if (reactApp === '/') {
-      res.sendFile(path.resolve(__dirname, '..', '..', 'public', 'index.html'));
-    } else {
-      const filename = path.resolve(compiler.outputPath, reactApp, 'index.html');
-      compiler.outputFileSystem.readFile(filename, (err, result) => {
-        if (err) {
-          return next(err);
-        }
-        res.set('content-type', 'text/html');
-        res.send(result);
-        res.end();
-        return 1;
-      });
-    }
-  });
+  app.use(webpackMiddleware);
 }
-
+app.get('/[a-z 0-9]+/', (req, res, next) => {
+  const reactApp = req.originalUrl;
+  if (reactApp === '/') {
+    res.sendFile(path.resolve(__dirname, '..', '..', 'public', 'index.html'));
+  } else {
+    const filename = path.resolve(compiler.outputPath, reactApp, 'index.html');
+    compiler.outputFileSystem.readFile(filename, (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.set('content-type', 'text/html');
+      res.send(result);
+      res.end();
+      return 1;
+    });
+  }
+});
 // Make our /dist folder static
 
 app.listen(1234, () => {

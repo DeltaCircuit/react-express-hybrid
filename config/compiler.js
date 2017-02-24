d98efdfb532898fb4869e1728b66cc660580df8e
Giridharan GM <giridharangm@outlook.com>
2017-02-24 14:51:21 +0530

    eslint compiler fix

--- config/compiler.js
+++ config/compiler.js
@@ -1,24 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies, no-console */
-var webpack = require('webpack');
-var chalk = require('chalk');
-var devConfig = require('./webpack.config.dev');
+const webpack = require('webpack');
+const chalk = require('chalk');
+const devConfig = require('./webpack.config.dev');
 
 const isLogEnabled = process.env.NODE_ENV ? process.env.NODE_ENV !== 'production' : true;
 const compiler = webpack(devConfig);
-compiler.plugin('invalid', function () {
+compiler.plugin('invalid', () => {
   if (isLogEnabled) {
     process.stdout.write(process.platform === 'win32' ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H');
     console.log(chalk.cyan('Compiling.. Please wait'));
   }
 });
 
-compiler.plugin('done', function (stats) {
+compiler.plugin('done', (stats) => {
   if (isLogEnabled) {
     process.stdout.write(process.platform === 'win32' ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H');
     if (stats.hasErrors()) {
       console.log(chalk.red('Errors found!'));
       const formattedStats = stats.toJson('errors-only');
-      formattedStats.errors.forEach(function (item) {
+      formattedStats.errors.forEach((item) => {
         console.log(item);
       });
       return;

281b2bda4763f3a4c5ebea5c34846d32840d7e0b
Giridharan GM <giridharangm@outlook.com>
2017-03-02 18:14:19 +0530

    Welcome page updated.

--- src/client/main/index.js
+++
@@ -1,5 +0,0 @@
-/* global document */
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<h1>Welcome to React + Express hybrid</h1>, document.getElementById('root'));
--- src/server/index.js
+++ src/server/index.js
@@ -14,26 +14,13 @@ const env = app.get('env');
 if (env === 'production') {
   // eslint-disable-next-line no-console
   console.log(chalk.red('WARNING!!!: Running in Production mode.'));
-  app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 }
-app.use(express.static(path.resolve(__dirname, '..', '..', 'dist', 'static')));
+app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
+
 // If no environment is specified, we'll assume its development env
 if (env !== 'production') {
   app.use(webpackMiddleware);
 }
-app.get('/[a-z 0-9]+/', (req, res, next) => {
-  const filename = path.resolve(compiler.outputPath, req.originalUrl, 'index.html');
-  compiler.outputFileSystem.readFile(filename, (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.set('content-type', 'text/html');
-    res.send(result);
-    res.end();
-    return 1;
-  });
-});
-// Make our /dist folder static
 
 app.listen(1234, () => {
   // eslint-disable-next-line no-console
